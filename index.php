b64d109f107ede833f89df660a0007f29b4d2c45
maksonSantos <santosmakson1+github@gmail.com>
2014-08-12 22:46:50 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Makson Santos</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="container">
+        <div class="col-md-6 header-container">
+            <img src="imag/logo.png" class="img-responsive" alt="Responsive Image">
+        </div>
+        <div class="col-md-6 header-container ">Linha 2</div>
+    </header>    
+    <nav class="navbar navbar-default nav-container" role="navigation">
+        <div class="container">
+            <ul class="nav nav-tabs ul-container" role="tablist">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Messages</a></li>
+            </ul>
+        </div>
+    </nav>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
